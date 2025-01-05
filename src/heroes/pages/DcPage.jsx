2dbb33fcbbba8a129e0aadb85c9918530bf9c3e6
CrisCantero15 <cristiancanterolopez@gmail.com>
2025-01-05 03:52:10 +0100

    Creo una App Heroes que permite visualizar cartas con información de varios superhéroes

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,14 @@
+import { HeroList } from '../components';
+
+export const DcPage = () => {
+    
+    
+    
+    return (
+        <>
+            <h1 className="mt-5">DC Comics</h1>
+            <hr />
+            <HeroList publisher="DC Comics" />
+        </>
+    )
+}
